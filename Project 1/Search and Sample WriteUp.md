b22cbfe5c91f68182190ec0dfdb27b9bcac5b548
jlpintolousinha <jlpintolousinha@gmail.com>
2017-10-12 02:01:07 +0200

    Project 1
    Initial commit

---
+++ Project 1/Search and Sample WriteUp.md
@@ -0,0 +1,64 @@
+## Project: Search and Sample Return
+
+**The goals / steps of this project were the following:**  
+
+**Training / Calibration**  
+
+* Download the simulator and take data in "Training Mode"
+* Test out the functions in the Jupyter Notebook provided
+* Add functions to detect obstacles and samples of interest (golden rocks)
+* Fill in the `process_image()` function with the appropriate image processing steps (perspective transform, color threshold etc.) to get from raw images to a map.  The `output_image` you create in this step should demonstrate that your mapping pipeline works.
+* Use `moviepy` to process the images in your saved dataset with the `process_image()` function.  Include the video you produce as part of your submission.
+
+**Autonomous Navigation / Mapping**
+
+* Fill in the `perception_step()` function within the `perception.py` script with the appropriate image processing functions to create a map and update `Rover()` data (similar to what you did with `process_image()` in the notebook). 
+* Fill in the `decision_step()` function within the `decision.py` script with conditional statements that take into consideration the outputs of the `perception_step()` in deciding how to issue throttle, brake and steering commands. 
+* Iterate on your perception and decision function until your rover does a reasonable (need to define metric) job of navigating and mapping.  
+
+[//]: # (Image References)
+
+[image1]: ./misc/rover_image.jpg
+[image2]: ./calibration_images/example_grid1.jpg
+[image3]: ./calibration_images/example_rock1.jpg 
+
+## [Rubric](https://review.udacity.com/#!/rubrics/916/view) Points
+### The rubric points will be individually mentioned at first. Each point will include a description of the implementation performed within, followed by an extended explanation in the subsequent sections.   
+
+#### 1. Rubric points 
+
+The proposed project was developed following these steps:
+1. All required software to properly deploy The Roversim simulator was installed and tested.  
+2. I familiarized myself with the simulator commands and the steps required to save data in a specific folder.
+3. The generated images were analized in order to identify terrain, obstacles and rocks in terms of a color threshold. 
+4. Changes were implemented in the coding so perspective transformation, color thresholding, coordinates transformation and processing of images could be performed smoothly. 
+5. The files `perception.py`, `drive_rover.py`, `decision.py` and `supporting_functions.py` were analized so the modifications previously performed on the `Rover_Project_Test_Notebook` were translated and adapted. 
+6. Functions `perception_step()` and `decision_step()` were modified in order to generate an adequate response on the autonomous mode of the rover model.
+7. It was possible not only to accomplish 
+
+### Notebook Analysis
+#### 1. Run the functions provided in the notebook on test images (first with the test data provided, next on data you have recorded). Add/modify functions to allow for color selection of obstacles and rock samples.
+Here is an example of how to include an image in your writeup.
+
+![alt text][image1]
+
+#### 1. Populate the `process_image()` function with the appropriate analysis steps to map pixels identifying navigable terrain, obstacles and rock samples into a worldmap.  Run `process_image()` on your test data using the `moviepy` functions provided to create video output of your result. 
+And another! 
+
+![alt text][image2]
+### Autonomous Navigation and Mapping
+
+#### 1. Fill in the `perception_step()` (at the bottom of the `perception.py` script) and `decision_step()` (in `decision.py`) functions in the autonomous mapping scripts and an explanation is provided in the writeup of how and why these functions were modified as they were.
+
+
+#### 2. Launching in autonomous mode your rover can navigate and map autonomously.  Explain your results and how you might improve them in your writeup.  
+
+**Note: running the simulator with different choices of resolution and graphics quality may produce different results, particularly on different machines!  Make a note of your simulator settings (resolution and graphics quality set on launch) and frames per second (FPS output to terminal by `drive_rover.py`) in your writeup when you submit the project so your reviewer can reproduce your results.**
+
+Here I'll talk about the approach I took, what techniques I used, what worked and why, where the pipeline might fail and how I might improve it if I were going to pursue this project further.  
+
+
+
+![alt text][image3]
+
+
---
+++ Project 1/decision.py
@@ -0,0 +1,143 @@
+import numpy as np
+import time
+
+
+# This is where you can build a decision tree for determining throttle, brake and steer 
+# commands based on the output of the perception_step() function
+def decision_step(Rover):
+
+    # Implement conditionals to decide what to do given perception data
+    # Here you're all set up with some basic functionality but you'll need to
+    # improve on this decision tree to do a good job of navigating autonomously!
+
+    # Example:
+    # Check if we have vision data to make decisions with 
+    if (Rover.nav_angles is not None):
+        # Check for Rover.mode status
+        if Rover.mode == 'forward': 
+            # Check the extent of navigable terrain
+            if len(Rover.nav_angles) >= Rover.stop_forward: #Rover.stop_forward:  
+                # If mode is forward, navigable terrain looks good 
+                # and velocity is below max, then throttle 
+                if Rover.vel < Rover.max_vel:
+                    # Set throttle value to throttle setting
+                    Rover.throttle = Rover.throttle_set
+
+                    if (Rover.throttle != 0 and Rover.vel == 0.0 and Rover.steer !=0) and Rover.vision_image[:,:,1].any() == False:
+                        Rover.rock_in_the_middle = True
+                    else:
+                        Rover.rock_in_the_middle = False
+                else: # Else coast
+                    Rover.throttle = 0
+                Rover.brake = 0
+                # Set steering to average angle clipped to the range +/- 15
+                Rover.steer = np.clip((np.mean(Rover.nav_angles * 180/np.pi))*0.5, -15, 15)
+                
+                if Rover.samples_collected == 6 and Rover.samples_to_find == 0:
+                    Rover.steer = np.clip(np.mean(Rover.starting_point_angle * 180/np.pi), -15, 15)     
+
+                if Rover.pos == Rover.starting_point:
+                    Rover.throttle = 0
+                    Rover.brake = Rover.brake_set*0.5
+                    Rover.mode = 'stop'                                                 
+            else:
+                Rover.throttle = 0
+                Rover.brake = Rover.brake_set*0.5
+                Rover.mode = 'stop'          
+                
+            # If there's a lack of navigable terrain pixels then go to 'stop' mode
+            if (len(Rover.nav_angles) < Rover.stop_forward) or Rover.rock_in_the_middle == True:
+                Rover.throttle = 0
+                Rover.brake = Rover.brake_set
+                Rover.mode = 'stop' 
+                                
+            if Rover.rock_angles != 0 and Rover.rock_dists != 0 and Rover.vel > 0.2:
+                Rover.throttle = 0
+                Rover.brake = Rover.brake_set*0.1
+                Rover.mode = 'rock' 
+
+        elif Rover.mode =='rock':
+            Rover.samples_located += 1
+            Rover.steer = np.clip(np.mean(Rover.rock_angles * 180/np.pi), -15, 15)
+            Rover.brake = 0
+            Rover.throttle = Rover.throttle_set*0.5
+            Rover.near = Rover.near_sample
+                 
+            if Rover.near == True:
+                Rover.brake = Rover.brake_set*2
+                Rover.throttle = 0
+            else:
+                if Rover.rock_angles > 0 :
+                    Rover.brake = 0
+                    Rover.steer = 15
+                else:
+                    Rover.brake = 0
+                    Rover.steer = -15
+                    
+            if Rover.vision_image[:,:,1].any() == False:
+                Rover.brake = 0
+                Rover.throttle = Rover.throttle_set
+                Rover.steer = np.clip((np.mean(Rover.nav_angles * 180/np.pi))*0.85, -15, 15)  
+                Rover.mode = 'forward'                 
+                    
+            #after collecting the rock, return to the initial state
+            if Rover.samples_collected > 0 and Rover.picked_rock == True:
+                Rover.brake = 0
+                Rover.throttle = Rover.throttle_set
+                Rover.steer = np.clip((np.mean(Rover.nav_angles * 180/np.pi))*1.5, -15, 15)
+                Rover.mode = 'forward'
+                Rover.picked_rock = False
+                               
+        # If we're already in "stop" mode then make different decisions
+        elif Rover.mode == 'stop':
+            # If we're in stop mode but still moving keep braking
+            if Rover.vel > 0.2:
+                Rover.throttle = 0
+                Rover.brake = Rover.brake_set*0.5
+                Rover.steer = 0
+            # If we're not moving (vel < 0.2) then do something else
+            elif Rover.vel <= 0.2:
+                # Now we're stopped and we have vision data to see if there's a path forward
+                if (len(Rover.nav_angles) < Rover.stop_forward) or Rover.rock_in_the_middle == True: 
+                    Rover.throttle = Rover.throttle = Rover.throttle_set*-1
+                    # Release the brake to allow turning
+                    Rover.brake = 0
+                    # Turn range is +/- 15 degrees, when stopped the next line will induce 4-wheel turning
+                    Rover.steer = 15 # Could be more clever here about which way to turn
+                # If we're stopped but see sufficient navigable terrain in front then go!              
+                if (len(Rover.nav_angles) >= Rover.go_forward):
+                    # Set throttle back to stored value
+                    Rover.throttle = Rover.throttle_set
+                    # Release the brake
+                    Rover.brake = 0
+                    # Set steer to mean angle
+                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
+                    Rover.mode = 'forward'
+                    Rover.rock_in_the_middle = False  
+                
+                if Rover.samples_collected == 6:
+                    Rover.throttle = 0
+                    Rover.brake = Rover.brake_set*0.5
+                    Rover.steer = 0                   
+                    
+
+    # Just to make the rover do something 
+    # even if no modifications have been made to the code
+    else:
+        Rover.throttle = Rover.throttle_set
+        Rover.steer = 0
+        Rover.brake = 0
+        
+    # If in a state where want to pickup a rock send pickup command
+    if Rover.near_sample and Rover.vel == 0 and not Rover.picking_up:
+        Rover.send_pickup = True
+        Rover.picked_rock = True
+                  
+    print(Rover.picked_rock)
+    print(Rover.rock_in_the_middle)
+    print(Rover.pos)
+    print(Rover.starting_point_dist,Rover.starting_point_angle)
+    return Rover
+
+
+
