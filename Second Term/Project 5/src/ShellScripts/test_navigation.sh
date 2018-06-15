98cbb840775e402197f6b42ec4a3f58e82a00833
jlpintolousinha <jlpintolousinha@gmail.com>
2018-06-14 19:48:35 -0500

    tes_navigation.sh modified
    Environment variables included

--- Second Term/Project 5/src/ShellScripts/test_navigation.sh
+++ Second Term/Project 5/src/ShellScripts/test_navigation.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/World/myMap.yaml
+export TURTLEBOT_3D_SENSOR=kinect
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/my_world_4" &
 sleep 5
 xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
